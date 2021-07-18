b9c4db0a43d026a4e2bc9b4da630fe9238c2fdbc
PabloLabG <pablico@gmail.com>
2021-07-18 12:13:35 +0200

    added

---
+++ Modulo1/Laboratorio/JS/demo.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("avatar").src = "../img/pato.png";
+document.getElementById("name").value = "Pablo";
+document.getElementById("lastName").value = "Laborde González";
+
+function fnConsole() {
+    console.log(("%c Apellido: " + document.getElementById("lastName").value), "background: black; color: green;");
+}
