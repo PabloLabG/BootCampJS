66731f0673f17be0e8ce8defe20049b65768d435
PabloLabG <pablico@gmail.com>
2021-07-26 22:25:49 +0200

    calculator 1.0

---
+++ Modulo4/JS/app.js
@@ -0,0 +1,59 @@
+/*
+* Funcion de Suma.
+*/
+var fSumar = (operadorA, operadorB) => {
+    let _result = 0;
+
+    if (!operadorA && !isNaN(operadorA) && (!operadorB && !isNaN(operadorB))) {
+        _result = Number(operadorA) + Number(operadorB);
+    }
+
+    document.getElementById("ResultOperacion").innerText = _result;    
+}
+
+/*
+* Funcion de Resta.
+*/
+var fRestar = (operadorA, operadorB) => {
+    let _result = 0;
+
+    if (!operadorA && !isNaN(operadorA) && (!operadorB && !isNaN(operadorB))) {
+        _result = Number(operadorA) - Number(operadorB);
+    }
+
+    document.getElementById("ResultOperacion").innerText = _result;    
+}
+
+/*
+* Funcion de Multiplicar.
+*/
+var fMultiplicar = (operadorA, operadorB) => {
+    let _result = 0;
+
+    if (!operadorA && !isNaN(operadorA) && (!operadorB && !isNaN(operadorB))) {
+        _result = Number(operadorA) * Number(operadorB);
+    }
+
+    document.getElementById("ResultOperacion").innerText = _result;    
+}
+
+/*
+* Funcion de Dividir.
+*/
+var fDividir = (operadorA, operadorB) => {
+    let _result = 0;
+
+    if (!operadorA && !isNaN(operadorA) && (!operadorB && !isNaN(operadorB))) {
+        _result = Number(operadorA) / Number(operadorB);
+    }
+
+    document.getElementById("ResultOperacion").innerText = _result;    
+}
+
+
+
+// Añadir los eventos Click
+document.getElementById("btnSumar").addEventListener("click", () => { fSumar(document.getElementById('operadorA').value, document.getElementById('operadorB').value); });
+document.getElementById("btnRestar").addEventListener("click", () => { fRestar(document.getElementById('operadorA').value, document.getElementById('operadorB').value); });
+document.getElementById("btnMultiplicar").addEventListener("click", () => { fMultiplicar(document.getElementById('operadorA').value, document.getElementById('operadorB').value); });
+document.getElementById("btnDividir").addEventListener("click", () => { fDividir(document.getElementById('operadorA').value, document.getElementById('operadorB').value); });
